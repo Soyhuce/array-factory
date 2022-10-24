1c841d40efc58dd05ff4d1c2428b8e8a5dcb4521
bastien-phi <bastien-phi@users.noreply.github.com>
2022-10-24 06:13:18 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1);
 
 uses()->beforeEach(function (): void {
-    set_error_handler(function ($level, $message, $file = '', $line = 0) {
-        if (in_array($level, [E_DEPRECATED, E_USER_DEPRECATED]) || (error_reporting() & $level)) {
+    set_error_handler(function ($level, $message, $file = '', $line = 0): void {
+        if (in_array($level, [E_DEPRECATED, E_USER_DEPRECATED], true) || (error_reporting() & $level)) {
             throw new ErrorException($message, 0, $level, $file, $line);
         }
     });
