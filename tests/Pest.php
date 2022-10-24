bc84ef5eabff86e4267cc6408324b71e14c55740
Bastien Philippe <bastien.philippe@soyhuce.fr>
2022-10-24 08:12:55 +0200

    Run tests without deprecation handling

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,1 +1,9 @@
 <?php declare(strict_types=1);
+
+uses()->beforeEach(function (): void {
+    set_error_handler(function ($level, $message, $file = '', $line = 0) {
+        if (in_array($level, [E_DEPRECATED, E_USER_DEPRECATED]) || (error_reporting() & $level)) {
+            throw new ErrorException($message, 0, $level, $file, $line);
+        }
+    });
+})->in(__DIR__);
